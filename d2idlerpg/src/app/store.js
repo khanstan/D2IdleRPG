5d2c6e9536eed96660bb3155c7d437348168bff8
khanstan <ithylight@gmail.com>
2021-12-11 12:45:30 +0200

    Implemented saving to cloud.

--- d2idlerpg/src/app/store.js
+++ d2idlerpg/src/app/store.js
@@ -26,24 +26,47 @@ const store = configureStore({
   preloadedState,
 })
 
-const saveToCloud = () => {
+const saveStateToCloud = () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "text/plain");
 
+  const payload = btoa(JSON.stringify(store.getState()));
+  const raw = `{\"base64\":\"${payload}\"}`;
+
+  const requestOptions = {
+    method: 'PATCH',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  try {
+    const data = JSON.stringify(store.getState());
+    console.log('Saving to cloud...');
+
+    fetch("https://d2idlerpg-default-rtdb.europe-west1.firebasedatabase.app/characters/sUfrTQhmbzdMqEDn08cFchoD6ql2/-MqVKvfWjUfIOaRO4-Sl.json", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log('Saved to cloud'))
+    .catch(error => console.log('Error while saving to cloud', error));
+  } catch (err) {
+    throw new Error('There was an error while trying to save the state to cloud')
+  }
 }
 
 const saveStateToLocalStorage = () => {
+
   try {
     console.log('Saving to local storage...');
     localStorage.setItem('reduxState', JSON.stringify(store.getState()));
-    console.log('Saved.');
   } catch (err) {
     throw new Error('There was an error while trying to save the state to localStorage')
   }
 }
 
-const throttledSaveToLocalStorage = throttle(() => saveStateToLocalStorage(), 1000);
-
+const throttledSaveStateToLocalStorage = throttle(() => saveStateToLocalStorage(), 1000);
+const throttledSaveStateToCloud = throttle(() => saveStateToCloud(), 10000)
 store.subscribe(() => {
-  throttledSaveToLocalStorage();
+  throttledSaveStateToLocalStorage();
+  throttledSaveStateToCloud();
 })
 
 export default store;
